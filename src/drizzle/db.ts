476156541060f727a71414103b5c96b1e14c92e2
Mutai001 <omusekim@gmail.com>
2024-07-23 12:10:44 +0300

    deploy error

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,10 +1,24 @@
+// import "dotenv/config";
+// import { drizzle, NodePgDatabase } from 'drizzle-orm/node-postgres';
+// import { Pool } from 'pg';
+// import * as schema from './schema';
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+// });
+
+// export const db: NodePgDatabase<typeof schema> = drizzle(pool, { schema, logger: true });
+
+
 import "dotenv/config";
-import { drizzle, NodePgDatabase } from 'drizzle-orm/node-postgres';
-import { Pool } from 'pg';
+import { drizzle, NeonHttpDatabase } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
 import * as schema from './schema';
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
+const databaseUrl = process.env.Database_URL as string;
+if (!databaseUrl) throw new Error("Database_URL is not set");
 
-export const db: NodePgDatabase<typeof schema> = drizzle(pool, { schema, logger: true });
+const sql = neon(databaseUrl);
+
+export const db: NeonHttpDatabase<typeof schema> = drizzle(sql, { schema, logger: true });
+
--- src/drizzle/migrate.ts
+++ src/drizzle/migrate.ts
@@ -1,25 +1,47 @@
-import 'dotenv/config';
-import { migrate } from 'drizzle-orm/node-postgres/migrator';
-import { db } from './db';
+// import 'dotenv/config';
+// import { migrate } from 'drizzle-orm/node-postgres/migrator';
+// import { db } from './db';
+
+// async function migration() {
+//   console.log('======== Migrations started ========');
+
+//   try {
+//     await migrate(db, {
+//         migrationsFolder: __dirname + "/migrations"
+//       });
+//     console.log('======== Migrations completed ========');
+//   } catch (err) {
+//     const error = err as Error;
+//     console.error('Migration error:', error.message);
+//   } finally {
+//     process.exit(0);
+//   }
+// }
+
+// migration().catch((err) => {
+//   const error = err as Error;
+//   console.error('Migration error:', error.message);
+//   process.exit(1);
+// });
+
+import { migrate } from "drizzle-orm/neon-http/migrator";
+import { db } from "./db";
 
 async function migration() {
-  console.log('======== Migrations started ========');
-
   try {
+    console.log("======Migration Started ======");
     await migrate(db, {
-        migrationsFolder: __dirname + "/migrations"
-      });
-    console.log('======== Migrations completed ========');
-  } catch (err) {
-    const error = err as Error;
-    console.error('Migration error:', error.message);
-  } finally {
+      migrationsFolder: __dirname + "/migrations"
+    });
+    console.log("======Migration Ended======");
     process.exit(0);
+  } catch (error) {
+    console.error("Migration failed with error: ", error);
+    process.exit(1);
   }
 }
 
-migration().catch((err) => {
-  const error = err as Error;
-  console.error('Migration error:', error.message);
+migration().catch((e) => {
+  console.error("Unexpected error during migration:", e);
   process.exit(1);
 });
